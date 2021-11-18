f95c6fb0f407be7496db591511b38072566fccb0
Hesedi3l <mathieucarallorca@gmail.com>
2021-11-19 00:33:13 +0100

    :hedgehog: add arrayConfig

--- source/config/fromScratchConfig.js
+++ source/config/fromScratchConfig.js
@@ -1,12 +1,85 @@
-const createDirectory = require('../utils/createDirectory.js')
-let fs = require("fs-extra");
+const { Listr } = require('listr2');
+const fs = require("fs");
+const path = require('path');
+/******************************************
+ * Function Tasks - Import
+ ******************************************/
+const waitTask = require('../utils/waitTask')
+const cloneRepo = require('../utils/cloneRepo')
+const copyFiles = require('../utils/copyFiles')
+const deleteFiles = require("../utils/deleteFiles");
+
+const arrayConfig = {
+    github: 'https://github.com/Hesedi3l/cli-creative',
+    githubPath : 'source/template/from-scratch',
+    mainDir: 'source',
+    fileDelete: ['.gitignore', 'package.json', 'package-lock.json', 'README.md'],
+}
+
 
 function fromScratchConfig(answers){
-    createDirectory(answers);
-    fs.copy('source/template/from-scratch', `${answers.name}`, function (err) {
-        if (err) return console.error(err)
-        console.log(`\x1b[32mApplication prête a l'emploi\x1b[0m`);
-    });
+    const tasks = new Listr(
+        [
+            {
+                title: 'Build from-scratch-app ...',
+                task: (context, task)=> {
+                    return task.newListr([
+                        {
+                            title: 'Clone repositories',
+                            task: async () => {
+                                try {
+                                    return await cloneRepo(answers, arrayConfig);
+                                } catch (err) {
+                                    console.log(err)
+                                }
+                            }
+                        },
+                        {
+                            title: 'Delete all files',
+                            task: async () => {
+                                try {
+                                    await deleteFiles(answers, arrayConfig);
+                                } catch (err) {
+                                    console.log(err)
+                                }
+                            }
+                        },
+                        {
+                            title: 'Copy files',
+                            task: async () => {
+                                try {
+                                    return await copyFiles(answers, arrayConfig);
+                                } catch (err) {
+                                    console.log(err)
+                                }
+                            }
+                        },
+                        {
+                            title: 'Remove source',
+                            task: async () => {
+                                try {
+                                    await waitTask(2);
+                                    return await fs.rmdirSync(path.join(process.cwd(), `${answers.name}/${arrayConfig.mainDir}`), {recursive: true});
+                                } catch (err) {
+                                    console.log(err)
+                                }
+                            }
+                        }
+                    ],{
+                        concurrent: false
+                    })
+                }
+            },
+        ],
+        {
+            concurrent: false
+        }
+    )
+    tasks.run();
 }
+
+
+
+
 
 module.exports = fromScratchConfig;
