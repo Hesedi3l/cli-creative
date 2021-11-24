3bcba263da5ab3d99a3844770de5ea668dfbae77
Hesedi3l <mathieucarallorca@gmail.com>
2021-11-24 21:29:00 +0100

    :bug: change multiplicator

--- source/utils/randomNameGenerator.js
+++ source/utils/randomNameGenerator.js
@@ -2,7 +2,7 @@ function randomNameGenerator(){
     const num = 8;
     let res = 'projet-';
     for(let i = 0; i < num; i++){
-        const random = Math.floor(Math.random() * 5);
+        const random = Math.floor(Math.random() * 6);
         res += String.fromCharCode(97 + random);
     }return res;
 }
